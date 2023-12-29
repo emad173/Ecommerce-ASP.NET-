431aae625638b4a81009035c2bc4365f8faafb5e
Emad <emadhassanali2020@gmail.com>
2023-12-28 16:56:42 -0800

    last edition

---
+++ OnlienShop/Areas/Admin/_models/UserRoleMaping.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlienShop.Areas.Admin._models
+{
+    public class UserRoleMaping
+    {
+        public string UserId { get; set; }
+        public string RoleId { get; set; }
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+    }
+}
